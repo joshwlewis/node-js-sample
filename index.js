77a8d58f743b7da0c1cab55d3ad000cd3a96a701
Josh W Lewis <josh.w.lewis@gmail.com>
2019-07-19 14:00:48 -0500

    bang

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('HA HA! BUSINESS!')
+  response.send('HA HA! BUSINESS!!')
 })
 
 app.listen(app.get('port'), function() {
